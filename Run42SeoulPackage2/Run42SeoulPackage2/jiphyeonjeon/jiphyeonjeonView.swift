2f380b653de875b8eab9ad0ab81dff808d09a724
chanhihi <chanhihi55@gmail.com>
2023-02-20 14:52:27 +0900

    :star: new: jiphyeonjeonView

---
+++ Run42SeoulPackage2/Run42SeoulPackage2/jiphyeonjeon/jiphyeonjeonView.swift
@@ -0,0 +1,20 @@
+//
+//  jiphyeonjeonView.swift
+//  Run42SeoulPackage2
+//
+//  Created by Chan on 2023/02/20.
+//
+
+import SwiftUI
+
+struct jiphyeonjeonView: View {
+    var body: some View {
+        Text("jip")
+    }
+}
+
+struct jiphyeonjeonView_Previews: PreviewProvider {
+    static var previews: some View {
+        jiphyeonjeonView()
+    }
+}
