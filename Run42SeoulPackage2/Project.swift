23ab8fdc5518eaf287b313e106c312e62d8f85be
chanhihi <chanhihi55@gmail.com>
2023-02-20 17:58:51 +0900

    :star: new: init Tuist with Project.swift

---
+++ Run42SeoulPackage2/Project.swift
@@ -0,0 +1,35 @@
+import ProjectDescription
+
+// MARK: Constants
+let projectName = "Run42SeoulPackage2"
+let organizationName = "Run42"
+let bundleID = "com.Run42SeoulPackage2"
+let targetVersion = "11.0"
+
+// MARK: Struct
+let project = Project(
+  name: projectName,
+  organizationName: organizationName,
+  packages: [],
+  settings: nil,
+  targets: [
+	Target(name: projectName,
+		   platform: .macOS,
+		   product: .app, // unitTests, .appExtension, .framework, dynamicLibrary, staticFramework
+		   bundleId: bundleID,
+		   deploymentTarget: .macOS(targetVersion: targetVersion),
+		   infoPlist: .file(path: "\(projectName)/src/Info.plist"),
+		   sources: ["\(projectName)/**"],
+		   resources: [],
+		   entitlements: "\(projectName)/src/Run42SeoulPackage2.entitlements",
+		   dependencies: [] // tuist generate할 경우 pod install이 자동으로 실행
+		  )
+  ],
+  schemes: [
+	Scheme(name: "\(projectName)-Debug"),
+	Scheme(name: "\(projectName)-Release")
+  ],
+  fileHeaderTemplate: nil,
+  additionalFiles: [],
+  resourceSynthesizers: []
+)
