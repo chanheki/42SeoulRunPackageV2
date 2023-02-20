3aa77d393deaccca10fbf1403c252de2703ed324
chanhihi <chanhihi55@gmail.com>
2023-02-20 14:52:36 +0900

    :star: new: intraView

---
+++ Run42SeoulPackage2/Run42SeoulPackage2/intra/intraView.swift
@@ -0,0 +1,20 @@
+//
+//  intraView.swift
+//  Run42SeoulPackage2
+//
+//  Created by Chan on 2023/02/20.
+//
+
+import SwiftUI
+
+struct intraView: View {
+    var body: some View {
+        Text("intra view")
+    }
+}
+
+struct intraView_Previews: PreviewProvider {
+    static var previews: some View {
+        intraView()
+    }
+}
