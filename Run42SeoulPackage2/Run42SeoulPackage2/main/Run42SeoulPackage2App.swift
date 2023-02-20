f2bf20175a08f349d5bfa77d93a3187291b1c666
chanhihi <chanhihi55@gmail.com>
2023-02-20 14:51:49 +0900

    :star: new: main

---
+++ Run42SeoulPackage2/Run42SeoulPackage2/main/Run42SeoulPackage2App.swift
@@ -0,0 +1,17 @@
+//
+//  Run42SeoulPackage2App.swift
+//  Run42SeoulPackage2
+//
+//  Created by Chan on 2023/02/16.
+//
+
+import SwiftUI
+
+@main
+struct Run42SeoulPackage2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
