6811817079dbb132a324b0911da7a0da0b9206e3
JJunghyunY <50796114+JJunghyunY@users.noreply.github.com>
2023-02-24 16:08:15 +0900

    :lipstick: Feature: add animations on button

--- Run42SeoulPackage2/Run42SeoulPackage2/main/MenuButtons.swift
+++ Run42SeoulPackage2/Run42SeoulPackage2/main/MenuButtons.swift
@@ -11,16 +11,39 @@ import SwiftUI
 struct MenuButtons: View {
     
     var title: String
+    var animation: Namespace.ID
+    @Binding var selected: String
+    
     var body: some View {
         
         Button {
-            // buttonAction func
+            withAnimation(.spring()) {
+                selected = title
+            }
+            
         } label: {
             HStack {
+                
                 Text(title)
+                    .fontWeight(selected == title ? .bold : .none)
+                    .foregroundColor(selected == title ? Color.white : Color.white.opacity(0.5))
+                    .animation(.none)
+                Spacer()
+                ZStack {
+                    Capsule()
+                        .fill(Color.clear)
+                        .frame(width: 3, height: 18)
+                    if selected == title {
+                        Capsule()
+                            .fill(Color.white)
+                            .frame(width: 3, height: 18)
+                            .matchedGeometryEffect(id: "Tab", in: animation)
+                    }
+                }
             }
-            .padding(.horizontal)
+            .padding(.leading)
         }
+        .buttonStyle(PlainButtonStyle())
         
     }
 //    func RedirectURL(url: String) { print("\(url)") }
